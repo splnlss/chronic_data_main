79e391f8add7194bb6961ae8fc8194831e3f8c98
splnlss <jon@splnlss.com>
2019-04-11 16:20:53 -0400

    display match.params.id

--- client/src/components/documentViewer/components/documentViewer.js
+++ client/src/components/documentViewer/components/documentViewer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import '../../../containers/dashboard/dashboard';
+import { fetchDocument} from '../action/fetch-document';
+import {API_BASE_URL} from '../../../config';
+
 import { Link } from 'react-router-dom'
 
 export class DocumentViewer extends React.Component {
 
   componentDidMount(){
+    const document = fetchDocument(this.props.match);
+    console.log(document)
     // this.props.fetchDocuments();
 
     // hypothetically getting a document
@@ -14,15 +19,32 @@ export class DocumentViewer extends React.Component {
 
   render(){
     const { documents, match } = this.props;
+    fetch(`${API_BASE_URL}/documents/${match.params.id}`, {
+      method: 'GET',
+      headers: {
+          'content-type': 'application/json'
+    },
+    body: JSON.stringify()
+  })
+  //   .then(res => normalizeResponseErrors(res))
+  .then(res => res.json())
+  .then(data => {
+      console.log('DATA:', data)
+    })
+    .catch(err => {
+        const {reason, message, location} = err;
+       
+      })
 
-    // match.params.id
+   
     
     return (
       <div>
         <p>Document Data</p>
-          {/* <div>
+          <div>
+            <h3>{match.params.id}</h3>
             <h3>{documents}</h3>
-          </div> */}
+          </div>
         <ul>
 
         </ul>
