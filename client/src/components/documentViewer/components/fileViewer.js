56d4cc7d449250035edd41b6f0965a6e105d61a5
splnlss <jon@splnlss.com>
2019-05-22 12:56:23 -0400

    changed link name in viewer

--- client/src/components/documentViewer/components/fileViewer.js
+++ client/src/components/documentViewer/components/fileViewer.js
@@ -4,13 +4,12 @@ import {AWS_BASE_URL} from '../../../config';
 export default function FileViewer(props){
   
     const imgUrl = AWS_BASE_URL+props.match.params.id;
-    console.log(imgUrl);
     return (
       <div>
             <div className="document">
               <img src={imgUrl} max-width="600"/>
               <br></br>
-              {imgUrl}
+              <a href={imgUrl}>Download Link</a>
             </div>
       </div>
     )
